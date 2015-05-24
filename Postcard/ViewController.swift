fbfeb8e7975440fab15e791222b0e508d749090d
Robert van Gorp <robertvangorp1@Roberts-MacBook.local>
2015-05-24 18:05:56 +0930

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // Message label below will show the hidden text field
+        messageLabel.hidden = false
+        
+        // Code below will display text from the Enter Message in the hidden label
+        messageLabel.text = enterMessageTextField.text
+        sender.backgroundColor = UIColor.redColor()
+        messageLabel.textColor = UIColor.redColor()
+        
+        // Removing the Enter Message field text
+        enterMessageTextField.text = ""
+        
+        // Now have the keyboard drop away
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Displayed Text", forState: UIControlState.Normal)
+        mailButton.backgroundColor = UIColor.blackColor()
+        
+    }
 
 }
 
